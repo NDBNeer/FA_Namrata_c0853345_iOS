f72ec8ad6003506cf079249c97ddcd8878166fff
Namrata <namrata.barot3@gmail.com>
2022-05-30 19:03:35 -0400

    adding initial data persistance structure

--- FA_Namrata_C0853345_iOS/Controller/CoreDataController.swift
+++ FA_Namrata_C0853345_iOS/Controller/CoreDataController.swift
@@ -10,5 +10,50 @@ import CoreData
 
 
 class CoreDataController{
+    func storeSquareStates(){
+        clearBoardStates()
+        
+    }
+    func storePlayerStates(){
+        clearPlayerStates()
+    }
+    func getSquareStates(){
+        
+    }
+    func getPlayerStates(){
+        
+    }
     
+    func clearBoardStates(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BoardState")
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                        context.delete(result)
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+    func clearPlayerStates(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "PlayerState")
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    context.delete(result)
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
 }
